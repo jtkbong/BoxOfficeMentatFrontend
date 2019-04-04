48b1dea925155fbd67c6ae1800c6d60922280588
Jeremy Bong <jeremybong@live.com>
2019-04-04 16:48:13 -0700

    other half of the last commit lol

--- Service/sqlhelper.py
+++
@@ -1,8 +0,0 @@
-import pymysql
-
-
-def get_sql_conn():
-    connection = pymysql.connect(host='boxofficementat.cwokc1guxfkk.us-west-2.rds.amazonaws.com',
-                                 user='jtkbong',
-                                 password='$andr0ckGundam')
-    return connection
