7170c40a690cd296fa6831c5a6c9daf4685f28ed
Jeremy Bong <jeremybong@live.com>
2019-04-29 18:25:25 -0700

    add a genre filter

--- Frontend/mentatfrontend/src/MovieResults.js
+++ Frontend/mentatfrontend/src/MovieResults.js
@@ -10,23 +10,29 @@ class MovieResults extends Component {
     }
 
     componentWillReceiveProps(props) {
-        var params = [];
-        if (props.title) {
-            params.push("title=" + props.title);
+        if (!(props.title === null && props.studio === null && props.releasedDate === null && props.genre === null)) {
+            console.log(props);
+            var params = [];
+            if (props.title) {
+                params.push("title=" + props.title);
+            }
+            if (props.studio) {
+                params.push("studio=" + props.studio);
+            }
+            if (props.genre) {
+                params.push("genre=" + props.genre);
+            }
+            if (props.releasedDate) {
+                params.push("releaseYear=" + props.releasedDate.getFullYear());
+                params.push("releaseMonth=" + (props.releasedDate.getMonth() + 1));
+                params.push("releaseDay=" + props.releasedDate.getDate());
+            }
+            fetch("http://boxofficementatservice-env.quumv36r5v.us-west-2.elasticbeanstalk.com/movies?" + params.join('&'), {mode: 'cors'})
+                .then(response => response.json())
+                .then(data => {
+                        this.setState({movies: data.movies});
+            });
         }
-        if (props.studio) {
-            params.push("studio=" + props.studio);
-        }
-        if (props.releasedDate) {
-            params.push("releaseYear=" + props.releasedDate.getFullYear());
-            params.push("releaseMonth=" + (props.releasedDate.getMonth() + 1));
-            params.push("releaseDay=" + props.releasedDate.getDate());
-        }
-		fetch("http://boxofficementatservice-env.quumv36r5v.us-west-2.elasticbeanstalk.com/movies?" + params.join('&'), {mode: 'cors'})
-			.then(response => response.json())
-			.then(data => {
-                    this.setState({movies: data.movies});
-        });
     }
 
 	render() {
@@ -39,6 +45,7 @@ class MovieResults extends Component {
                         <tr>
                             <th align='left'>Title</th>
                             <th align='left'>Studio</th>
+                            <th align='left'>Genre</th>
                             <th align='left'>Released Date</th>
                             <th align='left'>Domestic Gross</th>
                         </tr>
@@ -46,6 +53,7 @@ class MovieResults extends Component {
                         <tr key={movie.name}>
                             <td>{movie.name}</td>
                             <td>{movie.studio}</td>
+                            <td>{movie.genre}</td>
                             <td>{movie.releasedDate}</td>
                             <td align='right'>${movie.domesticGross}</td>
                         </tr>    
--- Frontend/mentatfrontend/src/MovieSearchPane.js
+++ Frontend/mentatfrontend/src/MovieSearchPane.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import MovieResults from './MovieResults';
 import DatePicker from 'react-datepicker';
+import Select from 'react-select';
 
 import "react-datepicker/dist/react-datepicker.css";
 
@@ -11,19 +12,45 @@ class MovieSearchPane extends Component {
 		this.state = {
 			title: null,
 			studio: null,
+			
 			selectedDate: new Date(2017, 3, 28),
-			releasedDate: null
+			releasedDate: null,
+
+			genreOptionsLoaded: false,
+			genres: [],
+			selectedGenre: null,
+			genre: null
 		};
+		
 		this.handleSearchClick = this.handleSearchClick.bind(this);
 		this.handleReleasedDateChange = this.handleReleasedDateChange.bind(this);
+		this.loadGenreOptions = this.loadGenreOptions.bind(this);
+	}
+
+	loadGenreOptions(input, callback) {
+		if (!this.state.genreOptionsLoaded) {
+			this.setState({ optionsLoaded: true });
+			fetch("http://boxofficementatservice-env.quumv36r5v.us-west-2.elasticbeanstalk.com/genres", {mode: 'cors'})
+				.then(response => response.json())
+				.then(data => {
+					let options = data.genres.map(genre => ({
+						value: genre,
+						label: genre
+					}));
+					this.setState({
+						genreOptionsLoaded: true,
+						genres: options
+					});
+				});
+		}
 	}
 	
 	handleSearchClick() {
-		console.log("HERE " + document.getElementById('releasedDate').selected);
 		this.setState({ 
 			title: document.getElementById('title').value,
 			studio: document.getElementById('studio').value,
-			releasedDate: this.state.selectedDate
+			releasedDate: this.state.selectedDate,
+			genre: this.state.selectedGenre.value
 		});
 	}
 
@@ -32,21 +59,43 @@ class MovieSearchPane extends Component {
 			selectedDate: date
 		});
 	}
-	
+
+	handleGenreOptionChange(option) {
+		this.setState({
+			selectedGenre: option
+		});
+	}
+
 	render() {
+	
+		const genreOptionsStyle = {
+			width: "400px"
+		};
+
 		return (
 			<div>
 				Title: <input type="text" id="title" />
 				<br/>
 				Studio: <input type="text" id="studio" />
 				<br/>
-				Release Date: <DatePicker id="releasedDate" 
+				Genre: 
+				<div style={genreOptionsStyle}><Select id="genre" 
+					options={this.state.genres}
+					value={this.state.selectedGenre}
+					onChange={(option) => this.handleGenreOptionChange(option)}
+					onFocus={this.loadGenreOptions}
+				/>
+				</div>
+				<br/>
+				Release Date: 
+				<DatePicker id="releasedDate" 
 					selected={this.state.selectedDate} 
 					onChange={this.handleReleasedDateChange}/>
 				<br/>
 				<button onClick={this.handleSearchClick}>Search</button>
 				<MovieResults 
-					title={this.state.title} studio={this.state.studio} releasedDate={this.state.releasedDate}/>
+					title={this.state.title} studio={this.state.studio}
+					releasedDate={this.state.releasedDate} genre={this.state.genre}/>
 			</div>
 		);
 	}
