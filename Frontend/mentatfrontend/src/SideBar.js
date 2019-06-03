357e3f59c6ee00f1e3fa354fc047f7ed9368b523
Jeremy Bong <jeremybong@live.com>
2019-06-02 22:40:05 -0700

    hide the test page

--- Frontend/mentatfrontend/src/SideBar.js
+++ Frontend/mentatfrontend/src/SideBar.js
@@ -15,7 +15,9 @@ class SideBar extends Component {
                     <li><Link to="/movies">Movies</Link></li>
                     <li><Link to="/people">People</Link></li>
                     <li><Link to="/comparer">Compare</Link></li>
+                    {/*
                     <li><Link to="/test">Test</Link></li>
+                    */}
                 </ul>
             </div>
         );
