e1745d6bf05ff9346cd0606be31422e61aa22a8e
Jeremy Bong <jeremybong@live.com>
2019-06-02 18:49:20 -0700

    box office comparer for any number of movies

--- Frontend/mentatfrontend/src/BoxOfficeComparer.js
+++ Frontend/mentatfrontend/src/BoxOfficeComparer.js
@@ -9,150 +9,149 @@ class BoxOfficeComparer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            movieIds: [],
-            movie1Id: "captainamerica",
-            movie2Id: "marvel14b",
-            movie1: null,
-            movie2: null,
-            size: [1200, 500]
+            movies: {},
+            size: [800, 400]
         };
         this.addMovieIdToList = this.addMovieIdToList.bind(this);
     }
 
-    componentDidMount() {
-        const url1 = process.env.REACT_APP_API_URL + "movie/" + this.state.movie1Id;
-        fetch(url1, { mode: 'cors' })
-            .then(response => response.json())
-            .then(data => {
-                this.setState({
-                    movie1: data
-                });
-            });
+    addMovieIdToList() {
+        const movieId = document.getElementById('movieId').value;
+        this.addMovie(movieId);
+    }
 
-        const url2 = process.env.REACT_APP_API_URL + "movie/" + this.state.movie2Id;
-        fetch(url2, { mode: 'cors' })
-            .then(response => response.json())
+    addMovie(movieId) {
+        const url = process.env.REACT_APP_API_URL + "movie/" + movieId;
+        fetch(url, { mode: 'cors' })
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    return -1;
+                }
+            })
             .then(data => {
-                this.setState({
-                    movie2: data
-                });
+                if (data !== -1) {
+                    this.setState(prevState => {
+                        prevState.movies[movieId] = data;
+                        return { movies: prevState.movies }
+                    });
+                }
             });
     }
 
-    addMovieIdToList() {
-        const movieId = document.getElementById('movieId').value;
-        const li = document.createElement('li');
-        li.innerHTML = movieId;
-        document.getElementById('movieIdList').appendChild(li);
-        this.setState(prevState => {
-            prevState.movieIds.push(movieId);
-            return {movieIds: prevState.movieIds}
-        });
+    getColor(num) {
+        const baseR = 15;
+        const baseG = 40;
+        const baseB = 200;
+        const newR = num * baseR % 255;
+        const newG = num * baseG % 255;
+        const newB = num * baseB % 255;
+        return "rgb(" + newR + "," + newG + "," + newB + ")";
     }
 
     render() {
-        if (this.state.movie1 && this.state.movie2) {
-            const movie1MaxWeekCount = d3.max(this.state.movie1.weeks, week => week.weekNumber);
-            const movie2MaxWeekCount = d3.max(this.state.movie2.weeks, week => week.weekNumber);
-            const maxWeekCount = Math.min(movie1MaxWeekCount, movie2MaxWeekCount);
-            const xScale = scaleLinear()
-                .domain([1, maxWeekCount + 1])
-                .range([0, this.state.size[0]]);
+        var movieEntries = Object.entries(this.state.movies);
 
-            const movie1MaxGross = d3.max(this.state.movie1.weeks, week => week.gross);
-            const movie2MaxGross = d3.max(this.state.movie2.weeks, week => week.gross);
-            const maxGross = Math.max(movie1MaxGross, movie2MaxGross);
-            const yScale = scaleLinear()
-                .domain([0, maxGross * 1.2])
-                .range([0, this.state.size[1]]);
+        var maxWeekCount = movieEntries.length > 0 ? 2000 : 0;
+        var maxWeekGross = 0;
 
-            const yScaleReverse = scaleLinear()
-                .domain([0, maxGross * 1.2])
-                .range([this.state.size[1], 0]);
+        if (movieEntries.length > 0) {
+            movieEntries.map(
+                ([movieId, movie]) => {
+                    const movieMaxWeekCount = d3.max(movie.weeks, week => week.weekNumber);
+                    maxWeekCount = Math.min(maxWeekCount, movieMaxWeekCount);
 
-            const widthPerBar = this.state.size[0] / (maxWeekCount * 2);
-
-            return (
-                <div>
-                    <React.Fragment>
-                        <div><svg width="15" height="15">
-                            <rect width="15" height="15" style={{ fill: "00BFFF" }} />
-                        </svg>{this.state.movie1.name} vs
-<svg width="15" height="15">
-                                <rect width="15" height="15" style={{ fill: "FABFFF" }} />
-                            </svg>{this.state.movie2.name}
-                        </div>
-                        <table>
-                            <tr><td>Movie ID</td>
-                            <td><input type="text" id="movieId" /></td>
-                            <td><button onClick={this.addMovieIdToList}>Add</button></td>
-                            </tr>
-                        </table>
-                        <ul id="movieIdList">
-
-                        </ul>
-                        
-
-                        <svg key="barChartGraphic" width={this.state.size[0] + 100} height={this.state.size[1] + 100}>
-                            <g transform="translate(80, 20)">
-                                {
-                                    this.state.movie1.weeks.map((week, i) => {
-                                        if (i < maxWeekCount) {
-                                            return <React.Fragment key={"fragment" + week.weekNumber}>
-                                                <rect key={"week" + week.weekNumber}
-                                                    x={widthPerBar * (week.weekNumber - 1)}
-                                                    y={this.state.size[1] - yScale(week.gross)}
-                                                    height={yScale(week.gross)}
-                                                    width={widthPerBar / 2}
-                                                    style={{ fill: "#00BFFF" }} >
-                                                </rect>
-                                                <text key={"weekText" + week.weekNumber}
-                                                    x={widthPerBar * (week.weekNumber - 1)}
-                                                    y={this.state.size[1] - yScale(week.gross)}
-                                                    fontSize="10"
-                                                    transform={"rotate(-90 " + widthPerBar * (week.weekNumber - 1) + " " + (this.state.size[1] - yScale(week.gross)) + ") translate(-90 10)"}
-                                                >
-                                                    {Util.intToDollarsText(week.gross)}
-                                                </text>
-                                            </React.Fragment>
-                                        }
-                                    }
-                                    )
-                                }
-                                {
-                                    this.state.movie2.weeks.map((week, i) => {
-                                        if (i < maxWeekCount) {
-                                            return <React.Fragment key={"fragment" + week.weekNumber}>
-                                                <rect key={"week" + week.weekNumber}
-                                                    x={widthPerBar * (week.weekNumber - 1) + (widthPerBar / 2)}
-                                                    y={this.state.size[1] - yScale(week.gross)}
-                                                    height={yScale(week.gross)}
-                                                    width={widthPerBar / 2}
-                                                    style={{ fill: "#FABFFF" }} >
-                                                </rect>
-                                                <text key={"weekText" + week.weekNumber}
-                                                    x={widthPerBar * (week.weekNumber - 1) + (widthPerBar / 2)}
-                                                    y={this.state.size[1] - yScale(week.gross)}
-                                                    fontSize="10"
-                                                    transform={"rotate(-90 " + widthPerBar * (week.weekNumber - 1) + (widthPerBar / 2) + " " + (this.state.size[1] - yScale(week.gross)) + ") translate(-90 10)"}
-                                                >
-                                                    {Util.intToDollarsText(week.gross)}
-                                                </text>
-                                            </React.Fragment>
-                                        }
-                                    }
-                                    )
-                                }
-                            </g>
-                        </svg>
-                    </React.Fragment>
-                </div>
-            );
-        } else {
-            return (
-                <div />
-            );
+                    const movieMaxWeekGross = d3.max(movie.weeks, week => week.gross);
+                    maxWeekGross = Math.max(maxWeekGross, movieMaxWeekGross);
+                })
         }
+
+        const xScale = scaleLinear()
+            .domain([1, maxWeekCount + 1])
+            .range([0, this.state.size[0]]);
+
+        const yScale = scaleLinear()
+            .domain([0, maxWeekGross * 1.2])
+            .range([0, this.state.size[1]]);
+
+        const yScaleReverse = scaleLinear()
+            .domain([0, maxWeekGross * 1.2])
+            .range([this.state.size[1], 0]);
+
+        const widthPerWeek = this.state.size[0] / maxWeekCount;
+        var movieCount = 0;
+        var movieColorCount = 0;
+
+        return (
+            <div>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>Movie ID</td>
+                            <td><input type="text" id="movieId" /></td>
+                            <td><button className="searchButtonStyle" onClick={this.addMovieIdToList}>Add</button></td>
+                        </tr>
+                    </tbody>
+                </table>
+                <ul id="movieIdList">
+                    {movieEntries.map(
+                        ([movieId, movie]) => {
+                            movieColorCount += 1;
+                            return (
+                                <li key={movie.id} style={{ listStyle: "none" }}>
+                                    <svg width={15} height={15} style={{ marginRight: "10px" }}>
+                                        <rect width={15} height={15} style={{ fill: this.getColor(movieColorCount) }} />
+                                    </svg>
+                                    {movie.name}
+                                </li>);
+                        }
+                    )}
+                </ul>
+                <React.Fragment>
+                    <svg key="barChartGraphic" width={this.state.size[0] + 100} height={this.state.size[1] + 100}>
+                        <g transform="translate(80, 20)">
+                            {
+                                movieEntries.map(([movieId, movie]) => {
+                                    movieCount += 1;
+                                    return (
+                                        movie.weeks.map(week =>
+                                            <React.Fragment key={"fragment" + movieId + week.weekNumber}>
+                                                <rect
+                                                    x={widthPerWeek * (week.weekNumber - 1) + (movieCount - 1) * (widthPerWeek / movieEntries.length)}
+                                                    y={this.state.size[1] - yScale(week.gross)}
+                                                    height={yScale(week.gross)}
+                                                    width={widthPerWeek / movieEntries.length - 1}
+                                                    style={{ fill: this.getColor(movieCount) }} >
+                                                </rect>
+                                                <text key={"weekText" + week.weekNumber}
+                                                    x={5 + widthPerWeek * (week.weekNumber - 1) + (movieCount - 1) * (widthPerWeek / movieEntries.length)}
+                                                    y={this.state.size[1] - yScale(week.gross)}
+                                                    fontSize="10"
+                                                    transform={"rotate(-30 " + (5 + widthPerWeek * (week.weekNumber - 1) + (movieCount - 1) * (widthPerWeek / movieEntries.length)) + " " + (this.state.size[1] - yScale(week.gross)) + ")"}
+                                                >
+                                                    {Util.intToDollarsText(week.gross)}
+                                                </text>
+                                            </React.Fragment>
+                                        )
+                                    );
+                                })
+                            }
+                        </g>
+                        <g transform="translate(80, 20)">
+                            {movieEntries.length > 0 &&
+                            <Axis key="yAxis" {...axisPropsFromTickScale(yScaleReverse, 4)} style={{ orient: LEFT }} />
+                            }
+                        </g>
+                        <g transform={"translate(" + (80 + widthPerWeek / 2) + ", " + (20 + this.state.size[1]) + ")"}>
+                            {movieEntries.length > 0 &&
+                            <Axis key="xAxis" {...axisPropsFromTickScale(xScale, maxWeekCount + 1)} style={{ orient: BOTTOM }} />
+                            }
+                        </g>
+                    </svg>
+                </React.Fragment>
+            </div>
+        );
     }
 }
 
