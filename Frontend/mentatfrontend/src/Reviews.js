6b04d7e03b54bff15e7d2b304723c03400350f18
Jeremy Bong <jeremybong@live.com>
2019-07-02 14:43:44 -0700

    sm ffh review

--- Frontend/mentatfrontend/src/Reviews.js
+++ Frontend/mentatfrontend/src/Reviews.js
@@ -10,6 +10,20 @@ class Reviews extends Component {
     render() {
         return (
             <div>
+                <h4>Spider-Man: Far From Home</h4><br />
+                <p>
+                I feel that I was somewhat disappointed with the movie. There were parts I really loved, specifically the high school romance and the fights with illusions. I was surprised at how relatable the Brad also going for MJ on the school trip thing was because I think I went through more or less the same thing lol. I had a huge crush on this girl and thought one of my best friends was also going after her (didn't help that he was bigger, more handsome, etc). The fights between Spidey and Mysterio were as awesome as you'd hope for with incredible mind-fuckery. The last fight with Mysterio is a huge callback to their first encounter in the comics where Peter has to trust his Spider-Sense to guide him. Seriously really awesome stuff there.
+</p>
+<p>
+The movie is solid entertainment, but I can't help but feel that they really missed out on the opportunity to do more interesting things with their narrative given the fallout of Endgame. I was counting on the high school cast to have a member that survived the snap but ultimately Brad's character was just a nuisance in Peter's way. I don't really like that Mr. Harrington's character is played almost exclusively for jokes because I think that if they had him be a survivor of the decimation, they could have done more interesting things with his relationship with the students.
+</p>
+<p>
+Lastly, Mysterio's reveal was ultimately a disappointment to me. They went with the angle that most fans saw coming from a mile away. The BARF connection is nice and makes a ton of sense, but I wish they didn't lean in so much on Tony being a jerk off-screen. I do think that ultimately the MCU version is very true to the 616 version, but given that this is the MCU and they tend to improve existing things, I was hoping there would be a less obvious twist like what they did with Adrian Toomes in Homecoming.
+</p>
+<p>
+3 / 5 Webs for me.
+                </p>
+
                 <h4>Aladdin (2019)</h4><br />
                 <p>
                     Will Smith makes the genie role his own, but the real star of the show is Jasmine played by Naomi Scott. The songs and singing are mostly as enjoyable as the original (which is either my favorite or second favorite animated Disney...idk lol), but the new solo Jasmine song "Speechless" is what at least sets this film apart from a cheap cash-grab remake. Do check it out because it's awesome and really showcases Scott's singing ability!
